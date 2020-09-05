6c61376128cd5916364f884f1c987840add50fb6
Hevedy <Hevedy@users.noreply.github.com>
2020-09-05 13:51:46 +0200

    C# port of LOD system, faster times up x10

--- Scripts/new_script.gd
+++ Scripts/new_script.gd
@@ -24,19 +24,25 @@
 # SOFTWARE.
 #==============================================================================
 
+tool
 extends MeshInstance
 class_name MeshInstaceLOD
 
-var LOD0_Inst
-var LOD1_Inst
-var LOD2_Inst
-var LOD3_Inst
+var LOD0_Inst#: MeshInstance
+var LOD1_Inst#: MeshInstance
+var LOD2_Inst#: MeshInstance
+var LOD3_Inst#: MeshInstance
 
 var LOD_Num
-export (int,-1,3) var LOD_Preview: int=0 setget set_LOD_Preview
+#export (int,1,4) var LOD_Num: int=1 setget set_LOD_Num
+export (int,-1,3) var LOD_Preview: int = 0 setget set_LOD_Preview
 export (float, 1.0,10000.0,0.01) var LOD1_Dist: float = 1000.0 setget set_LOD1_Dist
 export (float, 1.0,10000.0,0.01) var LOD2_Dist: float = 2000.0 setget set_LOD2_Dist
 export (float, 1.0,10000.0,0.01) var LOD3_Dist: float = 3000.0 setget set_LOD3_Dist
+#export (Mesh) var LOD0_Mesh: Mesh setget set_LOD0_Mesh
+#export (Mesh) var LOD1_Mesh: Mesh setget set_LOD1_Mesh
+#export (Mesh) var LOD2_Mesh: Mesh setget set_LOD2_Mesh
+#export (Mesh) var LOD3_Mesh: Mesh setget set_LOD3_Mesh
 
 
 var LOD: int = 0
@@ -44,8 +50,25 @@ var LODLast: int = 0
 var LODDistance: float = 0.0
 var LODDistanceLast: float = 0.0
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 func set_LOD_Preview(value: int):
 	LOD_Preview = value
+	#if Engine.editor_hint: set_LOD(value)
+	_ready()
+	#if not is_inside_tree():
+		#print("WTF") 
+		#yield(self, "ready")
+	#set_LOD(value)
+	return value
+	#if Engine.editor_hint:
+		#pass
+		#set_LOD(value)
+	#set_LOD(value)
+
+#func set_LOD_Num(value: int):
+#	LOD_Num = value
 	
 func set_LOD1_Dist(value: float):
 	LOD1_Dist = value
@@ -55,8 +78,21 @@ func set_LOD2_Dist(value: float):
 	
 func set_LOD3_Dist(value: float):
 	LOD3_Dist= value
+	
+# Set LODs
+#func set_LOD0_Mesh(value: Mesh):
+#	LOD0_Mesh= value
+	
+#func set_LOD1_Mesh(value: Mesh):
+#	LOD1_Mesh= value
+	
+#func set_LOD2_Mesh(value: Mesh):
+#	LOD2_Mesh= value
+	
+#func set_LOD3_Mesh(value: Mesh):
+#	LOD3_Mesh= value
 
-
+"""
 func find_node_by_name(root, name):
 	if(root.get_name() == name): return root
 	for child in root.get_children():
@@ -65,8 +101,10 @@ func find_node_by_name(root, name):
 		var found = find_node_by_name(child, name)
 		if(found): return found
 	return null
+"""
 
 func find_LODs():
+	#var ownerParent = get_owner()
 	for child in self.get_children():
 		if( "_LOD1" in child.get_name()):
 			LOD1_Inst = child
@@ -91,16 +129,41 @@ func find_LODs():
 		LOD0_Inst = MeshInstance.new()
 		LOD_Num = 0
 		
-	print(LOD_Preview)
+	#print(LOD_Preview)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#if not Engine.editor_hint:
+	#if self.has_node("SM_NotSoCube_LOD2"):
+	#	$SM_NotSoCube_LOD2.visible = false
+	#	print("LOD SET")
+	
+	#LOD0_Inst = MeshInstance.new()
+	#LOD1_Inst = MeshInstance.new()
+	#LOD2_Inst = MeshInstance.new()
+	#LOD3_Inst = MeshInstance.new()
 	find_LODs()
+	#if LOD0_Mesh:
+	#	LOD0_Inst.mesh = LOD0_Mesh.new()
+	#if LOD1_Mesh:
+	#	LOD1_Inst.mesh = LOD1_Mesh.new()
+	#if LOD1_Mesh:
+	#	LOD2_Inst.mesh = LOD2_Mesh.new()
+	#if LOD1_Mesh:
+	#	LOD3_Inst.mesh = LOD3_Mesh.new()
+	
+	#add_child(LOD0_Inst)
+	#add_child(LOD1_Inst)
+	#add_child(LOD2_Inst)
+	#add_child(LOD3_Inst)
 	
 	if LOD_Preview < 0:
-		set_Distance(self.global_transform.origin.distance_to( get_tree().get_viewport().get_camera().get_global_transform().origin))
+		#set_Distance(self.global_transform.origin.distance_to( get_tree().get_root().get_node("Main/Player").get_global_transform().origin ))
+		#set_Distance(self.global_transform.origin.distance_to( get_tree().get_viewport().get_camera().get_global_transform().origin))
+		set_Distance(1000.0)
 	else:
 		set_LOD(LOD_Preview)
+	#pass # Replace with function body.
 
 func set_Distance(value:float):
 	if LODDistance == value:
@@ -115,7 +178,7 @@ func set_Distance(value:float):
 		set_LOD(2)
 	elif value > LOD3_Dist: #LOD3
 		set_LOD(3)
-	else:
+	else: #LOD0
 		set_LOD(0)
 	
 
@@ -123,7 +186,9 @@ func set_LOD(value: int):
 	var a = clamp(value,0,LOD_Num-1)
 	if a < 0:
 		return
-
+	#if( LOD == a ):
+	#	return
+	
 	LODLast = LOD
 	match a:
 		0:
@@ -149,3 +214,6 @@ func set_LOD(value: int):
 			
 	LOD = a
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Scripts/new_scriptb.cs
@@ -0,0 +1,178 @@
+//==============================================================================
+// Godot HeXperiment by Hevedy
+// <https://github.com/Hevedy/GodotHeXperiment>
+// Godot HeXperiment MPL 2.0 Source Code
+// Copyright (C) 2020 Hevedy <https://github.com/Hevedy>.
+// This file is part of the Godot HeXperiment MPL 2.0 Source Code.
+// Godot HeXperiment MPL 2.0 Source Code is free software:
+// you can redistribute it and/or modify it under the terms of the
+// Mozilla Public License 2.0 as published by the Mozilla Foundation,
+// either version 2 of the License, or (at your option) any later version.
+// Godot HeXperiment MPL 2.0 Source Code is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the Mozilla Public License 2.0 for more details.
+// You should have received a copy of the Mozilla Public License 2.0
+// along with Godot HeXperiment MPL 2.0 Source Code.
+// If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//==============================================================================
+
+
+using Godot;
+using System;
+using System.Collections.Generic;
+
+[Tool]
+public class MeshInstaceLODNew : MeshInstance
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	static MeshInstance LOD0_Inst;
+	static MeshInstance LOD1_Inst;
+	static MeshInstance LOD2_Inst;
+	static MeshInstance LOD3_Inst;
+
+	static int LOD_Num = 0;
+	static int LOD = 0;
+	static int LODLast = 0;
+	static float LODDistance = 0.0f;
+	static float LODDistanceLast = 0.0f;
+	
+	private int _LOD_Preview = 0;
+	[Export] 
+	public int LOD_Preview 
+	{ 
+		get {	return _LOD_Preview; } 
+		set { 	_LOD_Preview = value; 
+				_Ready();
+			}
+	}
+
+	[Export] public float LOD1_Dist { get; set; } = 1000.0f;
+	[Export] public float LOD2_Dist { get; set; } = 2000.0f;
+	[Export] public float LOD3_Dist { get; set; } = 3000.0f;
+
+	void find_LODs() {
+		Godot.Collections.Array childrens = this.GetChildren();
+		foreach (Node child in childrens) {
+			string name = child.Name;
+			if( name.Contains("_LOD1") ) {
+				LOD1_Inst = (MeshInstance)child;
+			} else if( name.Contains("_LOD2") ) {
+				LOD2_Inst = (MeshInstance)child;
+			} else if( name.Contains("_LOD3") ) {
+				LOD3_Inst = (MeshInstance)child;
+			} else if( !name.Contains("_Coll") ) {
+				LOD0_Inst = (MeshInstance)child;
+			}
+		}
+		LOD_Num = 4;
+		if (LOD3_Inst == null) {
+			LOD3_Inst = new MeshInstance();
+			LOD_Num = 3;
+		}
+		if (LOD2_Inst== null) {
+			LOD2_Inst = new MeshInstance();
+			LOD_Num = 2;
+		}
+		if (LOD1_Inst == null) {
+			LOD1_Inst = new MeshInstance();
+			LOD_Num = 1;
+		}
+		if (LOD0_Inst == null) {
+			LOD0_Inst = new MeshInstance();
+			LOD_Num = 0;
+		}
+	}
+	
+	public void set_Distance( float _value ) {
+		
+		if ( LODDistance == _value) {
+			return;
+		}
+		
+		LODDistanceLast = LODDistance;
+		LODDistance = _value;
+		
+		if (_value > LOD1_Dist && _value < LOD2_Dist) { //LOD1
+			set_LOD(1);
+		} else if (_value > LOD2_Dist && _value < LOD3_Dist) { //LOD2
+			set_LOD(2);
+		} else if (_value > LOD3_Dist) { //LOD3
+			set_LOD(3);
+		} else { //LOD0
+			set_LOD(0);
+		}
+	}
+	
+
+	public void set_LOD( int _value ) {
+
+		int a = Mathf.Clamp(_value,0,LOD_Num-1);
+		if (a < 0) {
+			return;
+		}
+		LODLast = LOD;
+	
+		switch (a) {
+			case 0:
+				LOD1_Inst.Visible = false;
+				LOD2_Inst.Visible = false;
+				LOD3_Inst.Visible = false;
+				LOD0_Inst.Visible = true;
+				break;
+			case 1:
+				LOD0_Inst.Visible = false;
+				LOD2_Inst.Visible = false;
+				LOD3_Inst.Visible = false;
+				LOD1_Inst.Visible = true;
+				break;
+			case 2:
+				LOD0_Inst.Visible = false;
+				LOD1_Inst.Visible = false;
+				LOD3_Inst.Visible = false;
+				LOD2_Inst.Visible = true;
+				break;
+			case 3:
+				LOD0_Inst.Visible = false;
+				LOD1_Inst.Visible = false;
+				LOD2_Inst.Visible = false;
+				LOD3_Inst.Visible = true;
+				break;
+				
+		}
+		LOD = a;
+	}
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		find_LODs();
+		
+		if (LOD_Preview < 0) {
+			set_Distance(100.0f);
+		} else {
+			set_LOD(LOD_Preview);
+		}
+		GD.Print("Ready Set");
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+
+	
+}
